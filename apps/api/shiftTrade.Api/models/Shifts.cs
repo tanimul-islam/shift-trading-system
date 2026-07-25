c2b1d26473731eedc1133ca13b91991d551c137e
tanimul-islam <tanimulislamdihan@gmail.com>
2026-07-25 09:09:07 -0400

    (db): shifts model

---
+++ apps/api/shiftTrade.Api/models/Shifts.cs
@@ -0,0 +1,17 @@
+namespace shiftTrade.api.models;
+
+
+public class Shifts
+{
+    public Guid Id {get;set;} = Guid.NewGuid();
+    public Guid OrganizationId {get;set;} 
+    public Guid LocationId {get;set;}
+    public string PostedByUserId{get;set;} = String.Empty;
+
+    public DateTime ScheduleStartUtx {get;set;} 
+    public DateTime ScheduleEndUtc {get;set;}
+    public string Staus {get;set;}  ="Open";
+    public string? AcceptedByUserId {get;set;} 
+    public DateTime CreatedAtUtc {get;set;} = DateTime.UtcNow;
+    public DateTime? AcceptedAtUtc { get; set; }
+    }
