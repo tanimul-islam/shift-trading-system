61970a54aa5a4249324e1b7a6f0e01485b556cfd
tanimul-islam <tanimulislamdihan@gmail.com>
2026-07-27 21:42:53 -0400

    (api): admin endpoints added with "/debts"

--- apps/api/shiftTrade.Api/Endpoints/AdminEndpoints.cs
+++ apps/api/shiftTrade.Api/Endpoints/AdminEndpoints.cs
@@ -14,14 +14,14 @@ public static class AdminEndpoints
     {
         var admin = app.MapGroup("/api/admin").WithTags("Admin").RequireAuthorization();
 
-        app.MapGet("shifts", async(
+        app.MapGet("/shifts", async(
             string? status,
             Guid? locationId,
             ClaimsPrincipal principal,
             ApplicationDbContext db
         ) =>
         {
-            if(principal.TryGetCurrentUser(out var organizationId,out var userId)){
+            if(!principal.TryGetCurrentUser(out var organizationId,out var userId)){
                 return Results.Unauthorized();
         }
 
@@ -82,6 +82,64 @@ public static class AdminEndpoints
             }).ToListAsync();
 
             return Results.Ok(shiftList);
+
+        });
+
+
+      app.MapGet("/debts", async(
+            string? status,
+            Guid? locationId,
+            ClaimsPrincipal principal,
+            ApplicationDbContext db
+        ) =>
+        {
+            if(!principal.TryGetCurrentUser(out var organizationId,out var userId)){
+                return Results.Unauthorized();
+        }
+
+        var organizationRole = principal.FindFirst("organization_role")?.Value;
+
+        if(!string.Equals(
+            organizationRole,"Owner",StringComparison.OrdinalIgnoreCase))
+            {
+                return Results.Forbid();
+            }
+
+
+            var query = db.HoursDebts
+                .AsNoTracking()
+                .Where(debt =>
+                    debt.OrganizationId == organizationId);
+
+            if (!string.IsNullOrWhiteSpace(status))
+            {
+                query = query.Where(debt => 
+                debt.Status.ToLower() == status.ToLower());
+            }
+
+           if (!string.IsNullOrWhiteSpace(userId))
+    {
+        query = query.Where(debt =>
+            debt.CreditorUserId == userId ||
+            debt.DebitorUserId == userId);
+    }
+
+            var debtList = await query
+            .OrderByDescending(debt => debt.CreateAtUtc)
+            .Select (debt  => new
+            {
+                    debt.Id,
+                    debt.ShiftId,
+                    debt.CreditorUserId,
+                    debt.DebitorUserId,
+                    debt.HoursOwed,
+                    debt.RemainingHours,
+                    repaidHours = debt.HoursOwed - debt.RemainingHours,
+                    debt.Status,
+                    debt.CreateAtUtc
+            }).ToListAsync();
+
+            return Results.Ok(debtList);
 
         });
 
